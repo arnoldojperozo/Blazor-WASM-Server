20f5995444898dedbcb16409fdcc97328915d12b
Arnoldo Perozo <aperozo@mergeworld.com>
2021-12-15 18:03:31 -0600

    Module 12

---
+++ HiddenVilla.ServerApp/Model/BlazorRoom.cs
@@ -0,0 +1,9 @@
+﻿namespace HiddenVilla.ServerApp.Model;
+
+public class BlazorRoom
+{
+    public int Id { get; set; }
+    public string RoomName { get; set; }
+    public decimal Price { get; set; }
+    public bool IsActive { get; set; }
+}
